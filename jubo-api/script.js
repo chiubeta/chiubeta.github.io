7bf4c13a1e295ca8272b5d1d886faa4ea93340dd
YiChinChiu <chiubeta@gmail.com>
2020-10-08 18:44:34 +0800

    Add files via upload

--- jubo-api/script.js
+++ jubo-api/script.js
@@ -1,13 +1,14 @@
-fetch('https://smc.jubo.health/api/vitalLink/vitalsign', {
-    headers: {
-        'apikey': 'RRVMRC3-ZKGMGYA-NK59NSC-EE1A1AJ',
-        'content-type': 'application/json'
-    },
-    mode: 'cors'
-})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+var myHeaders = new Headers();
+myHeaders.append("apikey", "RRVMRC3-ZKGMGYA-NK59NSC-EE1A1AJ");
+myHeaders.append("Cookie", "__cfduid=da4350f70f39f87b464a4eb12751121ff1600653595");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://smc.jubo.health/api/vitalLink/organization/branch", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
