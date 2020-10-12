52e6328d487af10e67ed17a7c5409acc395f3165
YiChinChiu <chiubeta@gmail.com>
2020-10-12 10:10:45 +0800

    change to develop environment

--- jubo-api/script.js
+++ jubo-api/script.js
@@ -1,13 +1,27 @@
 var myHeaders = new Headers();
-myHeaders.append("apikey", "RRVMRC3-ZKGMGYA-NK59NSC-EE1A1AJ");
+myHeaders.append("apikey", "78A7HF2-N2E48GR-JFT7ARY-YB8P893");
 
 var requestOptions = {
   method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
+  headers: myHeaders
 };
 
-fetch("https://smc.jubo.health/api/vitalLink/organization/branch", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
+fetch("https://develop.jubo.health/api/vitalLink/vitalsign", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+    const dataList = result.map(item => `
+      <div class="card">
+        <div>住民 ID: ${item.patient._id}</div>
+        <div>體溫: ${item.TP || ''}</div>
+        <div>脈搏: ${item.PR || ''}</div>
+        <div>呼吸: ${item.RR || ''}</div>
+        <div>血壓: ${item.SYS || ''}/${item.DIA || ''}</div>
+        <div>血氧: ${item.SPO2 || ''}</div>
+        <div>疼痛: ${item.PAIN || ''}</div>
+      </div>
+    `).join("");
+
+    document.querySelector(".api-data-container").innerHTML = dataList;
+  })
   .catch(error => console.log('error', error));
